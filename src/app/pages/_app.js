e3fca00b207fa360441db3d5db169267d58c1998
martyan <martin.juzl@gmail.com>
2020-01-22 19:56:26 +0100

    next settings

--- src/app/pages/_app.js
+++ src/app/pages/_app.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import App, { Container } from 'next/app'
+import Head from 'next/head'
 import { Provider } from 'react-redux'
 import withRedux from 'next-redux-wrapper'
 import createStore from '../lib/store'
 
+let resizeTimeout = null
+const calculateVH = () => {
+    if(resizeTimeout) clearTimeout(resizeTimeout)
+    resizeTimeout = setTimeout(() => {
+        console.log('resize')
+        let vh = window.innerHeight * 0.01
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }, 200)
+}
+
 class MyApp extends App {
+
+    componentDidMount() {
+        calculateVH()
+        window.addEventListener('resize', calculateVH)
+    }
+
     render () {
         const { Component, pageProps, store } = this.props
 
         return (
-                <Container>
-                    <Provider store={store}>
+            <Container>
+                <Provider store={store}>
+                    <>
+                        <Head>
+                            <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+                        </Head>
                         <Component {...pageProps} />
-                    </Provider>
-                </Container>
+                    </>
+                </Provider>
+            </Container>
         )
     }
+
 }
 
 export default withRedux(createStore)(MyApp)
--- src/app/pages/_document.js
+++ src/app/pages/_document.js
@@ -1,10 +1,15 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
     static async getInitialProps(ctx) {
         const initialProps = await Document.getInitialProps(ctx)
 
-        return { ...initialProps }
+        const sheet = new ServerStyleSheet()
+        const page = ctx.renderPage(App => props => sheet.collectStyles(<App {...props} />))
+        const styleTags = sheet.getStyleElement()
+
+        return { ...initialProps, ...page, styleTags }
     }
 
     render() {
@@ -12,6 +17,7 @@ class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+                    {this.props.styleTags}
                 </Head>
                 <body>
                     <Main />
