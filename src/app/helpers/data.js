0ea9159cd6e08825df76915a68d3fc1890251b31
martyan <martin.juzl@gmail.com>
2020-01-29 00:31:18 +0100

    helpers/data

---
+++ src/app/helpers/data.js
@@ -0,0 +1,22 @@
+import moment from 'moment'
+
+export const getPlayersFromTeams = (teams) => {
+    return teams.reduce((acc, currVal) => {
+        const roster = currVal.roster.roster.map(roster => ({...roster, teamId: currVal.id}))
+        return [...acc, ...roster]
+    }, [])
+}
+
+export const getDateWZero = (d) => (d < 10 ? '0' : '') + d
+
+export const getDateText = (d) => {
+    const now = moment()
+    const today = moment.utc(`${now.get('year')}-${getDateWZero(now.get('month') + 1)}-${getDateWZero(now.get('date'))}`)
+    const diff = today.diff(d, 'days')
+
+    if(diff === 0) return 'Today'
+    if(diff === 1) return 'Yesterday'
+    if(diff > 2 && diff < 7) return 'This week'
+    if(now.get('year') > d.get('year')) return d.format('DD MMM YYYY')
+    return d.format('DD MMM')
+}
