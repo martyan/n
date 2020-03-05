b723e3d06249ebd8d5651201d9b002d52b641016
martyan <martin.juzl@gmail.com>
2020-03-05 22:09:43 +0100

    Rankings component

--- src/app/components/PlayerFeed.js
+++ src/app/components/PlayerFeed.js
@@ -2,12 +2,11 @@ import React, { useEffect, useRef } from 'react'
 import { useState } from 'react'
 import Game from './Game'
 import { Parallax, Background } from 'react-parallax'
-// import { getStats } from './TeamFeed'
-// import { getRankText, getStatName } from '../helpers/stats'
 import PlayerStats from './PlayerStats'
 import { goToTeamFeed } from '../helpers/navigation'
 import LoadMore from './LoadMore'
 import { getGameIdFromLink, getPlayersMedia } from '../helpers/data'
+import { Rankings } from './Rankings'
 
 const PlayerFeed = ({
     player,
@@ -60,31 +59,6 @@ const PlayerFeed = ({
     const noMore = loadedIndex === playerSchedule.length
     const notLoaded = !player || String(player.id) !== String(playerId)
 
-    // const isGoalie = player.primaryPosition.code === 'G'
-
-    // const seasonStats = getStats('statsSingleSeason', player.stats)
-    // const rankingStats = getStats('regularSeasonStatRankings', player.stats)
-
-    // const rankKeys = Object.keys(rankingStats)
-    // const ranks = rankKeys.map(key => {
-    //     const [ string, n, sup ] = /(\d+)([A-Za-z]+)/g.exec(rankingStats[key])
-    //     return {
-    //         key,
-    //         n: parseInt(n),
-    //         sup
-    //     }
-    // })
-
-    // const filteredRanks = ranks
-    //     .filter(rank => isGoalie ? rank.n <= 10 : rank.n <= 30)
-    //     .filter(rank => !(isGoalie && rank.key === 'penaltyMinutes'))
-
-    // const goodRanks = filteredRanks.sort((a, b) => {
-    //     if(a.n > b.n) return 1
-    //     if(a.n < b.n) return -1
-    //     return 0
-    // })
-
     return (
         <div className="player-feed">
             {!notLoaded && (
@@ -127,21 +101,7 @@ const PlayerFeed = ({
 
                     <PlayerStats player={player} />
 
-                    {/*<div className="ranks">
-                        {goodRanks.map(rank => {
-                            const value = seasonStats[getStatName(rank.key)]
-                            return (
-                                <div key={rank.key} className="rank">
-                                    <div className="position">
-                                        <span className="n">{rank.n}</span>
-                                        <span className="sup">{rank.sup}</span>
-                                    </div>
-                                    <div className="name">{getRankText(rank.key)}</div>
-                                    {typeof value !== 'undefined' && <div className="value">{value}</div>}
-                                </div>
-                            )
-                        })}
-                    </div>*/}
+                    <Rankings player={player} />
 
                     {playerSchedule.slice(0, loadedIndex).map(game => (
                         <div key={game.date} className="game">
