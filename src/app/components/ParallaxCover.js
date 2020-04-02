16084847d8bcbf2ac620d0a2cf37c759f666be84
martyan <martin.juzl@gmail.com>
2020-04-02 19:38:55 +0200

    Revert "parallax layer"
    This reverts commit 44fae4d9405df9e271e39e6c5b17352929075806.

--- src/app/components/ParallaxCover.js
+++ src/app/components/ParallaxCover.js
@@ -1,5 +1,5 @@
-import React, { Component, useState, useEffect, useRef } from 'react'
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import React, { useState, useEffect } from 'react'
+import { useSpring, animated, interpolate } from 'react-spring'
 
 import mantinel from '../static/img/mantinel.png'
 import lines from '../static/img/lines.png'
@@ -8,43 +8,60 @@ import tuukka from '../static/img/tuukka.png'
 import puck from '../static/img/puck.png'
 import panarin from '../static/img/panarin.png'
 
-class ParallaxCover extends Component {
-
-    render() {
-
-        // const canUseDOM = !!(typeof window !== 'undefined' && window.document && window.document.createElement)
-        //
-        // if(!canUseDOM) return null
-
-        return (
-            <div className="parallax-cover">
-                <Parallax pages={2} ref={ref => this.parallax = ref}>
-                    <ParallaxLayer offset={0} speed={-1.1} style={{ pointerEvents: 'none' }}>
-                        <img className="tuukka" src={tuukka} alt="" />
-                        <img className="panarin" src={panarin} alt="" />
-                    </ParallaxLayer>
-                    {/*<ParallaxLayer offset={1} speed={1} style={{ backgroundColor: '#805E73' }} />*/}
-                    {/*<ParallaxLayer offset={2} speed={1} style={{ backgroundColor: '#87BCDE' }} />*/}
-                    {/*<ParallaxLayer offset={0} speed={0.5}>*/}
-                        {/*<span onClick={() => this.parallax.scrollTo(1)}>Layers can contain anything</span>*/}
-                    {/*</ParallaxLayer>*/}
-                </Parallax>
-            </div>
-        )
+const debounce = (func, wait = 5, immediate = true) => {
+    let timeout
+    return function() {
+        const context = this
+        const args = arguments
+        const later = function() {
+            timeout = null
+            if (!immediate) func.apply(context, args)
+        }
+        const callNow = immediate && !timeout
+        clearTimeout(timeout)
+        timeout = setTimeout(later, wait)
+        if (callNow) func.apply(context, args)
     }
+}
 
-    // return (
-    //     <div className={cover ? 'cover active' : 'cover'} onClick={() => setCover(!cover)}>
-    //         <div className="inner">
-    //             <img className="mantinel" src={mantinel} alt="" />
-    //             <img className="lines" src={lines} alt="" />
-    //             <img className="net" src={net} alt="" />
-    //             <img className="tuukka" src={tuukka} alt="" />
-    //             <img className="puck" src={puck} alt="" />
-    //             <animated.img style={{ transform: interpHeader }} className="panarin" src={panarin} />
-    //         </div>
-    //     </div>
-    // )
+const ParallaxCover = () => {
+
+    const [ cover, setCover ] = useState(false)
+    const [ scrollY, setScrollY ] = useState(0)
+
+    useEffect(() => {
+        const handleScroll = () => setScrollY(window.scrollY)
+        window.addEventListener("scroll", debounce(handleScroll))
+
+        return () => window.removeEventListener("scroll", debounce(handleScroll))
+    }, [debounce])
+
+    const [ { springscrollY }, springsetScrollY ] = useSpring(() => ({
+        springscrollY: 0
+    }))
+
+    const parallaxLevel = 20
+    springsetScrollY({ springscrollY: scrollY })
+
+    const interpHeader = springscrollY.interpolate(
+        o => `translateX(${o / parallaxLevel}px) translateY(-${o / parallaxLevel}px)`
+    )
+
+    // const [{ st, xy }, set] = useSpring(() => ({ st: 0, xy: [0, 0] }))
+    // const interpPanarin = interpolate([st, xy], (o, xy) => `translate(${xy[0] / 25},${xy[1] / 25 + -10 + o / 8})`)
+
+    return (
+        <div className={cover ? 'cover active' : 'cover'} onClick={() => setCover(!cover)}>
+            <div className="inner">
+                <img className="mantinel" src={mantinel} alt="" />
+                <img className="lines" src={lines} alt="" />
+                <img className="net" src={net} alt="" />
+                <img className="tuukka" src={tuukka} alt="" />
+                <img className="puck" src={puck} alt="" />
+                <animated.img style={{ transform: interpHeader }} className="panarin" src={panarin} />
+            </div>
+        </div>
+    )
 
 }
 
