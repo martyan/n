24408881874861ecb3b1a153fed7635221526f1e
martyan <martin.juzl@gmail.com>
2020-05-14 17:36:14 +0200

    root reducer fix

--- src/app/lib/reducer.js
+++ src/app/lib/reducer.js
@@ -3,8 +3,8 @@ import auth, { initialState as initialStateAuth } from './auth/reducer'
 import app, { initialState as initialStateApp } from './app/reducer'
 
 export const initialState = {
-    ...initialStateAuth,
-    ...initialStateApp
+    auth: initialStateAuth,
+    app: initialStateApp
 }
 
 export default combineReducers({
